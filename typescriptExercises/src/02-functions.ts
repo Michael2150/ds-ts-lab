2f73403e9b67a47df34e98549f87739b2f68d4df
Michael Gerber <52236517+Michael2150@users.noreply.github.com>
2023-09-18 11:33:19 +0100

    Function to update friend's interests.

--- typescriptExercises/src/02-functions.ts
+++ typescriptExercises/src/02-functions.ts
@@ -62,3 +62,14 @@ function findFriends(friends: Friend[], predicate: (f: Friend) => boolean) {
 
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+
+function addInterest(friend: Friend, interest: string) {
+  if (friend.interests) {
+    friend.interests.push(interest);
+  } else {
+    friend.interests = [interest];
+  }
+  return friend.interests;
+}
+
+console.log(addInterest(friends[0], 'Politics'))
